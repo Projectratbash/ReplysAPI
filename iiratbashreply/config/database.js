ea04f3ba11bd313422df762eaa82c68bb041144c
Eddie-Sc <109636301+Eddie-Sc@users.noreply.github.com>
2022-09-22 15:07:52 +1200

    Add files via upload

---
+++ iiratbashreply/config/database.js
@@ -0,0 +1,8 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = "mongodb+srv://james123:james123@cluster0.u7qiil6.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  if (err) throw err;
+});
+const collection = client.db("ratdb").collection("reply");
+module.exports = {collection, ObjectId}
